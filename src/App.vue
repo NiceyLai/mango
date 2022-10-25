225bc36f1b3a1e9a228405d3fb848a21273799d7
nicey <laisi1127@163.com>
2022-10-25 14:40:12 +0800

    完成+1操作学会使用script setup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+const onclick = () => {
+  count.value += 1;
+};
+</script>
+
 <template>
   <div>
-    hi
+    {{ count }}
   </div>
+  <button @click="onclick">+1</button>
 </template>
+
+
+
+<style></style>
