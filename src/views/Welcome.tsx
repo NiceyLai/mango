ef9c9c85b841a582f0c491cea4b21112fc3c0c43
nicey <laisi1127@163.com>
2022-10-26 16:47:49 +0800

    feature: 第一个欢迎界面

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,15 +1,15 @@
 import { defineComponent } from 'vue';
 import { RouterView } from 'vue-router';
 import s from './Welcome.module.scss'
-import logo from '../assets/icons/mango.svg'
+import logo from '../assets/icons/mangosteen.svg'
 export const Welcome = defineComponent({
   setup: (props, context) => {
     return () => <div class={s.wrapper}>
       <header>
         <img src={logo} />
-        <h1>芒果记账</h1>
+        <h1>山竹记账</h1>
       </header>
-      <main><RouterView /></main>
+      <main class={s.main}><RouterView /></main>
     </div>
   }
 })
