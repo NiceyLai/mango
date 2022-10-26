dad40bd3c590474cb78ef2472f40556006679983
nicey <laisi1127@163.com>
2022-10-26 14:06:20 +0800

    feature: 创建欢迎界面的页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -8,16 +8,16 @@ import { Foo } from "../views/Foo";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
-  { path: "/", component: Foo },
-  { path: "/about", component: Bar },
+  { path: '/', component: Foo },
+  { path: '/about', component: Bar },
   {
-    path: "/welcome",
+    path: '/welcome',
     component: Welcome,
     children: [
-      { path: "/1", component: First },
-      { path: "/2", component: Second },
-      { path: "/3", component: Third },
-      { path: "/4", component: Forth },
-    ],
-  },
+      { path: '1', component: First, },
+      { path: '2', component: Second, },
+      { path: '3', component: Third, },
+      { path: '4', component: Forth, },
+    ]
+  }
 ];
