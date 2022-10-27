9aca00fa8ace825c3b36d7c11b6fb8771fac4e20
nicey <laisi1127@163.com>
2022-10-27 15:53:25 +0800

    究极重构

--- src/components/welcome/first.tsx
+++ src/components/welcome/first.tsx
@@ -1,29 +1,32 @@
-import { defineComponent } from "vue";
 import s from "./WelcomeLayout.module.scss";
 import pig from "../../assets/icons/pig.svg";
-import { RouterLink } from "vue-router";
 import { WelcomeLayout } from "./WelcomeLayout";
-export const First = defineComponent({
-  setup: (props, context) => {
-    const slots = {
-      icon: () => <img class={s.icon} src={pig} />,
-      title: () => (
-        <h2>
-          会赚钱
-          <br />
-          更要会省钱
-        </h2>
-      ),
-      buttons: () => (
-        <>
-          <RouterLink class={s.fake} to="/start">
-            跳过
-          </RouterLink>
-          <RouterLink to="/welcome/2">下一页</RouterLink>
-          <RouterLink to="/start">跳过</RouterLink>
-        </>
-      ),
-    };
-    return () => <WelcomeLayout v-slots={slots}></WelcomeLayout>;
-  },
-});
+import { RouterLink } from "vue-router";
+import { FunctionalComponent } from "vue";
+export const First: FunctionalComponent = () => {
+  return (
+    <WelcomeLayout>
+      {{
+        icon: () => <img class={s.icon} src={pig} />,
+        title: () => (
+          <h2>
+            会挣钱
+            <br />
+            还要会省钱
+          </h2>
+        ),
+        buttons: () => (
+          <>
+            <RouterLink class={s.fake} to="/start">
+              跳过
+            </RouterLink>
+            <RouterLink to="/welcome/2">下一页</RouterLink>
+            <RouterLink to="/start">跳过</RouterLink>
+          </>
+        ),
+      }}
+    </WelcomeLayout>
+  );
+};
+
+First.displayName = "First";
