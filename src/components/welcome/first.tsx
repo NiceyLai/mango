9137daed460cddd8f7c610d28af77c75d5cf148e
nicey <laisi1127@163.com>
2022-10-27 15:38:13 +0800

    重构：提取 WelcomeLayout 组件

--- src/components/welcome/first.tsx
+++ src/components/welcome/first.tsx
@@ -1,22 +1,29 @@
-import { defineComponent } from 'vue';
-import s from './First.module.scss';
-import pig from '../../assets/icons/pig.svg';
-import { RouterLink } from 'vue-router';
+import { defineComponent } from "vue";
+import s from "./WelcomeLayout.module.scss";
+import pig from "../../assets/icons/pig.svg";
+import { RouterLink } from "vue-router";
+import { WelcomeLayout } from "./WelcomeLayout";
 export const First = defineComponent({
   setup: (props, context) => {
-    return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img src={pig} />
-          <h2>会挣钱<br/>还要会省钱</h2>
-        </div>
-        <div class={s.actions}>
-          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-          <RouterLink to="/welcome/2" >下一页</RouterLink>
-          <RouterLink to="/start" >跳过</RouterLink>
-        </div>
-      </div>
-    )
-  }
-})
- 
+    const slots = {
+      icon: () => <img class={s.icon} src={pig} />,
+      title: () => (
+        <h2>
+          会赚钱
+          <br />
+          更要会省钱
+        </h2>
+      ),
+      buttons: () => (
+        <>
+          <RouterLink class={s.fake} to="/start">
+            跳过
+          </RouterLink>
+          <RouterLink to="/welcome/2">下一页</RouterLink>
+          <RouterLink to="/start">跳过</RouterLink>
+        </>
+      ),
+    };
+    return () => <WelcomeLayout v-slots={slots}></WelcomeLayout>;
+  },
+});
--- src/components/welcome/forth.tsx
+++ src/components/welcome/forth.tsx
@@ -1,21 +1,22 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss';
-import clock from '../../assets/icons/clock.svg';
+import s from './WelcomeLayout.module.scss';
+import cloud from '../../assets/icons/cloud.svg';
 import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
 export const Forth = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img class={s.icon} src={clock} />
-          <h2>每日提醒<br/>不遗漏每一笔账单</h2>
-        </div>
-        <div class={s.actions}>
+      <WelcomeLayout>
+        {{ 
+          icon: ()=> <img class={s.icon} src={cloud} />,
+          title: () => <h2>每日提醒<br/>不遗漏每一笔账单</h2>,
+          buttons: () => <>
           <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
           <RouterLink to="/start" >完成</RouterLink>
           <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-        </div>
-      </div>
+          </>
+        }}
+      </WelcomeLayout>
     )
   }
 })
