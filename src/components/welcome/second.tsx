444fee886164900d7012b2f5b6364205a05ce9d1
nicey <laisi1127@163.com>
2022-10-28 11:56:20 +0800

    使用 SVG Sprites

--- src/components/welcome/second.tsx
+++ src/components/welcome/second.tsx
@@ -1,12 +1,11 @@
 import { defineComponent } from "vue";
 import s from "./welcome.module.scss";
-import clock from "../../assets/icons/clock.svg";
-import { RouterLink } from "vue-router";
-import { WelcomeLayout } from "./WelcomeLayout";
 import { FunctionalComponent } from 'vue';
 export const Second: FunctionalComponent = () => {
   return <div class={s.card}>
-    <img class={s.icon} src={clock} />
+    <svg>
+      <use xlinkHref='#clock'></use>
+    </svg>
     <h2>每日提醒<br />不遗漏每一笔账单</h2>
   </div>
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+// @ts-nocheck
+import { svgstore } from './src/vite_plugins/svgstore';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,5 +13,6 @@ export default defineConfig({
       transformOn: true,
       mergeProps: true,
     }),
+    svgstore(),
   ],
 });
