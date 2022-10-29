5d88f371977f8d56ee1e625594017dd3f0bc4a2b
nicey <laisi1127@163.com>
2022-10-29 22:03:53 +0800

    添加 Navbar 组件

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,7 +1,7 @@
 import { defineComponent, defineProps, PropType } from 'vue';
 import s from './Icon.module.scss';
 
-export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig' | 'mango'
+export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig' | 'mango' | 'menu'
 
 export const Icon = defineComponent({
   props: {
