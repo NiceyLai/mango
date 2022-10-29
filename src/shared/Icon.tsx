68f40c2f86f838e22ab2c50599bf38511cba7119
nicey <laisi1127@163.com>
2022-10-29 17:42:58 +0800

    修改加号不居中bug

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,19 +1,17 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, defineProps, PropType } from 'vue';
 import s from './Icon.module.scss';
-
 export const Icon = defineComponent({
-    props: {
-        name: {
+  props: {
+    name: {
             type:String as PropType<'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig' | 'mango'>
-        }
-    },
+      
+    }
+  },
   setup: (props, context) => {
     return () => (
-      <div>
-            <svg class ={s.icon}>
-                <use xlinkHref={'#'+ props.name}></use>
-            </svg>
-      </div>
+      <svg class={s.icon}>
+        <use xlinkHref={'#' + props.name}></use>
+      </svg>
     )
   }
 })
