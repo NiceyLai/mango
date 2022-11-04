e9d905c25a6b944e78137ffb94cc6bdfa5361f7a
nicey <laisi1127@163.com>
2022-11-04 16:25:18 +0800

    完成记一笔的样式

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { defineComponent } from 'vue';
 import { Navbar } from '../shared/Navbar';
+import s from './MainLayout.module.scss';
 export const MainLayout = defineComponent({
     setup: (props, context) => {
         return () => (
@@ -7,7 +8,7 @@ export const MainLayout = defineComponent({
                 <Navbar>
                     {{
                         default: () => context.slots.title?.(),
-                        icon: () =>  context.slots.icon?.()
+                        icon: () => context.slots.icon?.()
                     }}
                 </Navbar>
                 {context.slots.default?.()}
