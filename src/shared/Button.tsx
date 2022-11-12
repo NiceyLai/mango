c321d98d79a0b9fdce890ee7dc6b2545711d5dd2
nicey <laisi1127@163.com>
2022-11-12 11:35:21 +0800

    Button 可以响应 click 了

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -14,12 +14,13 @@ export const Button = defineComponent({
       default: 'important'
     },
     type: {
-      type: String as PropType<'submit' | 'button'>
+      type: String as PropType<'submit' | 'button'>,
+      // default: 'button'
     }
   },
   setup: (props, context) => {
     return () =>
-      <button type={props.type} class={[s.button, s[props.level]]}>
+      <button type={props.type} class={[s.button, s[props.level]]} onClick={props.onClick}>
         {context.slots.default?.()}
       </button>;
   },
