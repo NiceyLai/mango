86a1c7195a4c557953616902564e262d4e0a1583
nicey <laisi1127@163.com>
2022-11-14 15:41:18 +0800

    避免重复请求

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { defineComponent, PropType, reactive, ref } from 'vue';
+import { useBool } from '../hooks/useBool';
 import { MainLayout } from '../layouts/MainLayout';
 import { Button } from '../shared/Button';
 import { Form, FormItem } from '../shared/Form';
@@ -18,6 +19,7 @@ export const SignInPage = defineComponent({
       code: []
     })
     const refValidationCode = ref<any>()
+    const { ref: refDisabled, toggle, on: disabled, off: enable } = useBool(false)
     const onSubmit = (e: Event) => {
       e.preventDefault()
       Object.assign(errors, {
@@ -36,9 +38,11 @@ export const SignInPage = defineComponent({
       throw error
     }
     const onClickSendValidationCode = async () => {
+      disabled()
       const response = await http
         .post('/validation_codes', { email: formData.email })
         .catch(onError)
+        .finally(enable)
       // 成功
       refValidationCode.value.startCount()
     }
@@ -60,6 +64,7 @@ export const SignInPage = defineComponent({
                 <FormItem label="验证码" type="validationCode"
                   placeholder='请输入六位数字'
                   countFrom={3}
+                  disabled={refDisabled.value}
                   onClick={onClickSendValidationCode}
                   v-model={formData.code} error={errors.code?.[0]} />
                 <FormItem style={{ paddingTop: '96px' }}>
