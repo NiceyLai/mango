7b3566a3f7903ed379828c098ebfe851c9491e89
nicey <laisi1127@163.com>
2022-11-15 11:09:09 +0800

    如果看过广告，就不用看了

--- src/components/welcome/FirstActions.tsx
+++ src/components/welcome/FirstActions.tsx
@@ -1,13 +1,14 @@
 import s from "./welcome.module.scss";
 import { RouterLink } from "vue-router";
 import { FunctionalComponent } from "vue";
+import { SkipFeatures } from "../../shared/SkipFeatures";
 export const FirstActions: FunctionalComponent = () => {
   return (
     <div class={s.actions}>
-    <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-    <RouterLink to="/welcome/2" >下一页</RouterLink>
-    <RouterLink to="/start" >跳过</RouterLink>
-  </div>
+      <SkipFeatures class={s.fake} />
+      <RouterLink to="/welcome/2" >下一页</RouterLink>
+      <SkipFeatures />
+    </div>
   );
 };
 
