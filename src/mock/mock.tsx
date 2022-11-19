100f07f54da798431d90cb64a58bf29d6c0c0c2f
nicey <laisi1127@163.com>
2022-11-19 23:02:44 +0800

    利用mock随机生成Tags

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -12,57 +12,24 @@ export const mockSession: Mock = (config) => {
 }
 
 export const mockTagIndex: Mock = (config) => {
-  if (config.params.kind === 'expenses') {
-    return [200, {
-      resources: [
-        { id: 1, name: '餐费', sign: '￥', kind: 'expenses' },
-        { id: 2, name: '打车', sign: '￥', kind: 'expenses' },
-        { id: 3, name: '聚餐', sign: '￥', kind: 'expenses' },
-        { id: 4, name: '打车', sign: '￥', kind: 'expenses' },
-        { id: 5, name: '聚餐', sign: '￥', kind: 'expenses' },
-        { id: 6, name: '打车', sign: '￥', kind: 'expenses' },
-        { id: 7, name: '聚餐', sign: '￥', kind: 'expenses' },
-      ]
-    }]
-  } else {
-    return [200, {
-      resources: [
-        { id: 4, name: '工资', sign: '￥', kind: 'income' },
-        { id: 5, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 6, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 11, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 18, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 17, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 19, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 4, name: '工资', sign: '￥', kind: 'income' },
-        { id: 5, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 6, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 11, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 18, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 17, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 19, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 4, name: '工资', sign: '￥', kind: 'income' },
-        { id: 5, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 6, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 11, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 18, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 17, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 19, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 4, name: '工资', sign: '￥', kind: 'income' },
-        { id: 5, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 6, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 11, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 18, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 17, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 19, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 4, name: '工资', sign: '￥', kind: 'income' },
-        { id: 5, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 6, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 11, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 18, name: '滴滴', sign: '￥', kind: 'income' },
-        { id: 17, name: '彩票', sign: '￥', kind: 'income' },
-        { id: 19, name: '滴滴', sign: '￥', kind: 'income' },
-      ]
-    }]
+  let id = 0
+  const createId = () => {
+    id += 1
+    return id
   }
+  const createTag = (n = 1, attrs?: any) =>
+    Array.from({ length: n }).map(() => ({
+      id: createId(),
+      name: faker.lorem.word(),
+      sign: faker.internet.emoji(),
+      kind: config.params.kind,
+      ...attrs
+    }))
+
+  if (config.params.kind === 'expenses') {
+    return [200, { resources: createTag(8) }]
+  } else {
+    return [200, { resources: createTag(20) }]
+  }
+
 }
