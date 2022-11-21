73119d0b0b7189c41bb8c4bd87a8efc0fa6a43ba
nicey <laisi1127@163.com>
2022-11-21 10:39:34 +0800

    实现选择tag

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -13,6 +13,7 @@ export const ItemCreate = defineComponent({
   },
   setup: (props, context) => {
     const refKind = ref('支出')
+    const refTagId = ref<number>()
     return () => (
       <MainLayout class={s.layout}>{{
         title: () => '记一笔',
@@ -21,10 +22,10 @@ export const ItemCreate = defineComponent({
           <div class={s.wrapper}>
             <Tabs v-model:selected={refKind.value} class={s.tabs}>
               <Tab name="支出">
-                <Tags kind='expenses' />
+                <Tags kind='expenses' v-model:selected={refTagId.value} />
               </Tab>
               <Tab name="收入">
-                <Tags kind='income' />
+                <Tags kind='income' v-model:selected={refTagId.value} />
               </Tab>
             </Tabs>
             <div class={s.inputPad_wrapper}>
