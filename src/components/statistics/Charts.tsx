2b4158d8215b5cd9a613dd0f6f03a5f0955e2b9b
nicey <laisi1127@163.com>
2022-12-03 22:07:00 +0800

    完成饼图

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,16 +1,17 @@
-import { computed, defineComponent, onMounted, PropType, ref } from 'vue';
-import { FormItem } from '../../shared/Form';
-import { http } from '../../shared/Http';
-import { Time } from '../../shared/time';
-import { Bars } from './Bars';
-import s from './Charts.module.scss';
-import { LineChart } from './LineChart';
-import { PieChart } from './PieChart';
-
+import { computed, defineComponent, onMounted, PropType, ref } from 'vue'
+import { FormItem } from '../../shared/Form'
+import s from './Charts.module.scss'
+import { LineChart } from './LineChart'
+import { PieChart } from './PieChart'
+import { Bars } from './Bars'
+import { http } from '../../shared/Http'
+import { Time } from '../../shared/time'
 const DAY = 24 * 3600 * 1000
 
-type Data1Item = { happen_at: string, amount: number }
+type Data1Item = { happen_at: string; amount: number }
 type Data1 = Data1Item[]
+type Data2Item = { tag_id: number; tag: Tag; amount: number }
+type Data2 = Data2Item[]
 export const Charts = defineComponent({
   props: {
     startDate: {
@@ -26,36 +27,64 @@ export const Charts = defineComponent({
     const kind = ref('expenses')
     const data1 = ref<Data1>([])
     const betterData1 = computed<[string, number][]>(() => {
-      if (!props.startDate || !props.endDate) { return [] }
+      if (!props.startDate || !props.endDate) {
+        return []
+      }
       const diff = new Date(props.endDate).getTime() - new Date(props.startDate).getTime()
       const n = diff / DAY + 1
       return Array.from({ length: n }).map((_, i) => {
         const time = new Time(props.startDate + 'T00:00:00.000+0800').add(i, 'day').getTimestamp()
         const item = data1.value[0]
-        const amount = (item && new Date(item.happen_at).getTime() === time)
-          ? data1.value.shift()!.amount
-          : 0
+        const amount = item && new Date(item.happen_at).getTime() === time ? data1.value.shift()!.amount : 0
         return [new Date(time).toISOString(), amount]
       })
     })
-
     onMounted(async () => {
-      const response = await http.get<{ groups: Data1, summary: number }>('/items/summary', {
+      const response = await http.get<{ groups: Data1; summary: number }>('/items/summary', {
         happen_after: props.startDate,
         happen_before: props.endDate,
         kind: kind.value,
+        group_by: 'happen_at',
         _mock: 'itemSummary'
       })
       data1.value = response.data.groups
+      console.log(data1.value)
     })
+
+    // data2
+
+    const data2 = ref<Data2>([])
+    const betterData2 = computed<{ name: string; value: number }[]>(() =>
+      data2.value.map((item) => ({
+        name: item.tag.name,
+        value: item.amount
+      }))
+    )
+
+    onMounted(async () => {
+      const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
+        happen_after: props.startDate,
+        happen_before: props.endDate,
+        kind: kind.value,
+        group_by: 'tag_id',
+        _mock: 'itemSummary'
+      })
+      data2.value = response.data.groups
+    })
+
     return () => (
       <div class={s.wrapper}>
-        <FormItem label='类型' type="select" options={[
-          { value: 'expenses', text: '支出' },
-          { value: 'income', text: '收入' }
-        ]} v-model={kind.value} />
+        <FormItem
+          label="类型"
+          type="select"
+          options={[
+            { value: 'expenses', text: '支出' },
+            { value: 'income', text: '收入' }
+          ]}
+          v-model={kind.value}
+        />
         <LineChart data={betterData1.value} />
-        <PieChart />
+        <PieChart data={betterData2.value} />
         <Bars />
       </div>
     )
--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -1,27 +1,46 @@
 import { faker } from '@faker-js/faker'
-import { AxiosRequestConfig } from 'axios';
+import { AxiosRequestConfig } from 'axios'
 
 type Mock = (config: AxiosRequestConfig) => [number, any]
 
-faker.setLocale('zh_CN');
+faker.setLocale('zh_CN')
 
-export const mockItemSummary: Mock = config => {
-  return [200, {
-    "groups": [
-      { "happen_at": "2022-07-18T00:00:00.000+0800", "amount": 100 },
-      { "happen_at": "2022-07-22T00:00:00.000+0800", "amount": 300 },
-      { "happen_at": "2022-07-29T00:00:00.000+0800", "amount": 200 }
-    ],
-    "summary": 600
-  }]
+export const mockItemSummary: Mock = (config) => {
+  if (config.params.group_by === 'happend_at') {
+    return [
+      200,
+      {
+        groups: [
+          { happen_at: '2022-07-18T00:00:00.000+0800', amount: 100 },
+          { happen_at: '2022-07-22T00:00:00.000+0800', amount: 300 },
+          { happen_at: '2022-07-29T00:00:00.000+0800', amount: 200 }
+        ],
+        summary: 600
+      }
+    ]
+  } else {
+    return [
+      200,
+      {
+        groups: [
+          { tag_id: 1, tag: { id: 1, name: '交通' }, amount: 100 },
+          { tag_id: 2, tag: { id: 2, name: '吃饭' }, amount: 300 },
+          { tag_id: 3, tag: { id: 3, name: '购物' }, amount: 200 }
+        ],
+        summary: 600
+      }
+    ]
+  }
 }
-
-export const mockItemIndexBalance: Mock = config => {
-  return [200, {
-    expenses: 9900,
-    income: 9900,
-    balance: 0
-  }]
+export const mockItemIndexBalance: Mock = (config) => {
+  return [
+    200,
+    {
+      expenses: 9900,
+      income: 9900,
+      balance: 0
+    }
+  ]
 }
 
 export const mockItemIndex: Mock = (config) => {
@@ -31,10 +50,9 @@ export const mockItemIndex: Mock = (config) => {
   const createPaper = (page = 1) => ({
     page,
     per_page,
-    count,
+    count
   })
-  const createTag = (attrs?: any) =>
-  ({
+  const createTag = (attrs?: any) => ({
     id: createId(),
     name: faker.lorem.word(),
     sign: faker.internet.emoji(),
@@ -68,21 +86,8 @@ export const mockItemIndex: Mock = (config) => {
     return [200, {}]
   }
 }
-
-export const mockTagEdit: Mock = config => {
-  const createTag = (attrs?: any) =>
-  ({
-    id: createId(),
-    name: faker.lorem.word(),
-    sign: faker.internet.emoji(),
-    kind: 'expenses',
-    ...attrs
-  })
-  return [200, { resource: createTag() }]
-}
-export const mockTagShow: Mock = config => {
-  const createTag = (attrs?: any) =>
-  ({
+export const mockTagEdit: Mock = (config) => {
+  const createTag = (attrs?: any) => ({
     id: createId(),
     name: faker.lorem.word(),
     sign: faker.internet.emoji(),
@@ -92,27 +97,44 @@ export const mockTagShow: Mock = config => {
   return [200, { resource: createTag() }]
 }
 
-export const mockItemCreate: Mock = config => {
-  return [200, {
-    resource: {
-      "id": 2264,
-      "user_id": 1312,
-      "amount": 9900,
-      "note": null,
-      "tags_id": [3508],
-      "happen_at": "2020-10-29T16:00:00.000Z",
-      "created_at": "2022-07-03T15:35:56.301Z",
-      "updated_at": "2022-07-03T15:35:56.301Z",
-      "kind": "expenses"
+export const mockTagShow: Mock = (config) => {
+  const createTag = (attrs?: any) => ({
+    id: createId(),
+    name: faker.lorem.word(),
+    sign: faker.internet.emoji(),
+    kind: 'expenses',
+    ...attrs
+  })
+  return [200, { resource: createTag() }]
+}
+
+export const mockItemCreate: Mock = (config) => {
+  return [
+    200,
+    {
+      resource: {
+        id: 2264,
+        user_id: 1312,
+        amount: 9900,
+        note: null,
+        tags_id: [3508],
+        happen_at: '2020-10-29T16:00:00.000Z',
+        created_at: '2022-07-03T15:35:56.301Z',
+        updated_at: '2022-07-03T15:35:56.301Z',
+        kind: 'expenses'
+      }
     }
-  }]
+  ]
+}
+export const mockSession: Mock = (config) => {
+  return [
+    200,
+    {
+      jwt: faker.random.word()
+    }
+  ]
 }
 
-export const mockSession: Mock = (config) => {
-  return [200, {
-    jwt: faker.random.word()
-  }]
-}
 let id = 0
 const createId = () => {
   id += 1
