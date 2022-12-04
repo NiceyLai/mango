f87c9824857665c78056e58420e378694b2acd37
nicey <laisi1127@163.com>
2022-12-04 23:17:36 +0800

    提示「敬请期待」

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -16,6 +16,7 @@ import { TagEdit } from "../components/tag/TagEdit";
 import { TagPage } from "../views/TagPage";
 import { SignInPage } from "../views/SignInPage";
 import { StatisticsPage } from "../views/StatisticsPage";
+import { ComingSoon } from "../shared/ComingSoon";
 
 export const routes: RouteRecordRaw[] = [
   { path: "/", redirect: "/welcome" },
@@ -68,5 +69,9 @@ export const routes: RouteRecordRaw[] = [
   },
   {
     path: '/statistics', component: StatisticsPage
+  }, {
+    path: '/export', component: ComingSoon
+  }, {
+    path: '/notify', component: ComingSoon
   }
 ];
