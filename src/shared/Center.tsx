4e54c8df4e43414eb010aa41bf28e24ee0c83050
nicey <laisi1127@163.com>
2022-12-12 21:55:33 +0800

    添加「目前没有数据」的提示

--- src/shared/Center.tsx
+++ src/shared/Center.tsx
@@ -1,10 +1,10 @@
 import { defineComponent, PropType } from 'vue';
 import s from './Center.module.scss';
 const directionMap = {
-    '-': 'horizontal',
-    '|': 'vertical',
-    'horizontal': 'horizontal',
-    'vertical': 'vertical'
+    '-': s.horizontal,
+    '|': s.vertical,
+    'horizontal': s.horizontal,
+    'vertical': s.vertical
 }
 export const Center = defineComponent({
     props: {
@@ -16,7 +16,7 @@ export const Center = defineComponent({
     setup: (props, context) => {
         const extraClass = directionMap[props.direction]
         return () => (
-            <div class={[s.center,extraClass]}>
+            <div class={[s.center, extraClass]}>
                 {context.slots.default?.()}
             </div>
         )
