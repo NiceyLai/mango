b029ed28a9cb3cf61089804ced8361ab90d24468
nicey <laisi1127@163.com>
2022-12-12 16:16:44 +0800

    修改 base

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,30 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import styleImport, { VantResolve } from "vite-plugin-style-import";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import styleImport, { VantResolve } from 'vite-plugin-style-import'
 
 // @ts-nocheck
-import { svgstore } from "./src/vite_plugins/svgstore";
+import { svgstore } from './src/vite_plugins/svgstore'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://mangosteen-test-3.oss-cn-hangzhou.aliyuncs.com',
   plugins: [
     vue(),
     vueJsx({
       transformOn: true,
-      mergeProps: true,
+      mergeProps: true
     }),
     svgstore(),
     styleImport({
-      resolves: [VantResolve()],
-    }),
+      resolves: [VantResolve()]
+    })
   ],
   server: {
     proxy: {
-      "/api/v1": {
-        target: "http://121.196.236.94:3000/",
-      },
-    },
-  },
-});
+      '/api/v1': {
+        target: 'http://121.196.236.94:3000/'
+      }
+    }
+  }
+})
