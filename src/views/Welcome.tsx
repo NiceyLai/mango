37e789e654533a06102dfd8d49dbabfcb0d15312
nicey <laisi1127@163.com>
2022-12-13 12:22:54 +0800

    重构了一下代码

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -16,57 +16,47 @@ const pushMap: Record<string, string> = {
   'Welcome3': '/welcome/4',
   'Welcome4': '/items',
 }
+
+type Params = { Component: VNode, route: RouteLocationNormalizedLoaded }
 export const Welcome = defineComponent({
   setup: (props, context) => {
-    const main = ref<HTMLElement>();
-    const route = useRoute();
-    const router = useRouter();
-    const { direction, swiping } = useSwipe(main, {
-      beforeStart: e => e.preventDefault()
-    });
+    const main = ref<HTMLElement>()
+    const { direction, swiping } = useSwipe(main, { beforeStart: e => e.preventDefault() })
+    const route = useRoute()
+    const router = useRouter()
     const replace = throttle(() => {
       const name = (route.name || 'Welcome1').toString()
       router.replace(pushMap[name])
-    }, 500);
+    }, 500)
     watchEffect(() => {
-      if (swiping.value && direction.value === "left") {
+      if (swiping.value && direction.value === 'left') {
         replace()
       }
-    });
-    return () => (
-      <div class={s.wrapper}>
-        <header>
-          <svg>
-            <use xlinkHref="#mangosteen"></use>
-          </svg>
-          <h1>山竹记账</h1>
-        </header>
-        <main class={s.main} ref={main}>
-          <RouterView name="main">
-            {({
-              Component: X,
-              route: R,
-            }: {
-              Component: VNode;
-              route: RouteLocationNormalizedLoaded;
-            }) => (
-              <Transition
-                enterFromClass={s.slide_fade_enter_from}
-                enterActiveClass={s.slide_fade_enter_active}
-                leaveToClass={s.slide_fade_leave_to}
-                leaveActiveClass={s.slide_fade_leave_active}
-              >
-                {X}
-              </Transition>
-            )}
-          </RouterView>
-        </main>
-        <footer>
-          <RouterView name="footer" />
-        </footer>
-      </div>
-    );
-  },
-});
-
+    })
+    return () => <div class={s.wrapper}>
+      <header>
+        <svg>
+          <use xlinkHref='#mangosteen'></use>
+        </svg>
+        <h1>山竹记账</h1>
+      </header>
+      <main class={s.main} ref={main}>
+        <RouterView name="main">
+          {({ Component: comp }: Params) =>
+            <Transition
+              enterFromClass={s.slide_fade_enter_from}
+              enterActiveClass={s.slide_fade_enter_active}
+              leaveToClass={s.slide_fade_leave_to}
+              leaveActiveClass={s.slide_fade_leave_active}>
+              {comp}
+            </Transition>
+          }
+        </RouterView>
+      </main>
+      <footer>
+        <RouterView name="footer" />
+      </footer>
+    </div>
+  }
+})
 export default Welcome
