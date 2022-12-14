60268458580a39272412bc0e674b7972f7061621
nicey <laisi1127@163.com>
2022-12-14 17:57:56 +0800

    列表收入支出用不同颜色展示

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -96,8 +96,11 @@ export const ItemSummary = defineComponent({
                     <div class={s.text}>
                       <div class={s.tagAndAmount}>
                         <span class={s.tag}>{item.tags && item.tags.length > 0 ? item.tags[0].name : '未分类'}</span>
-                        <span class={s.amount}>
-                          ￥<Money value={item.amount} />
+                        <span class={[
+                          item.kind === 'expenses' ? s.expensesAmount : s.incomeAmount
+                        ]}>
+                          ￥
+                          <Money value={item.amount} />
                         </span>
                       </div>
                       <div class={s.time}>
